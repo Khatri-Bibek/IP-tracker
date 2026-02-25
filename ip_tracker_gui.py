d35d2b976010e522960bc8467eff9cd3e6fded53
Khatri-Bibek <7cbbkyxcjp@privaterelay.appleid.com>
2026-02-25 12:21:04 +0545

    added title for my programme

--- ip_tracker_gui.py
+++ ip_tracker_gui.py
@@ -5,4 +5,7 @@ root.title("IP Tracker")
 root.geometry("450x400")
 root.resizable(False, False)
 
+title_label = tk.Label(root, text="IP Address Tracker", font=("Arial", 18, "bold"))
+title_label.pack(pady=10)
+
 root.mainloop()
