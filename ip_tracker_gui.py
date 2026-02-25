e5f4fe4483c4c5979f48f0f1076a456e8e9451a6
Khatri-Bibek <7cbbkyxcjp@privaterelay.appleid.com>
2026-02-25 12:22:13 +0545

    Add Track IP button

--- ip_tracker_gui.py
+++ ip_tracker_gui.py
@@ -12,4 +12,7 @@ tk.Label(root, text="Enter IP Address:").pack(pady=5)
 entry = tk.Entry(root, width=30, font=("Arial", 12))
 entry.pack(pady=5)
 
+track_button = tk.Button(root, text="Track IP")
+track_button.pack(pady=10)
+
 root.mainloop()
